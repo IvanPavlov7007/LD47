dd459336b03d736c8b87461fe63a999fb4485620
IvanPavlov <ivanpavlov7007@gmail.com>
2020-10-02 16:22:51 +0200

    Simpliest Dialog UI added
    You can make Dialog vie Asset create new Dialog. Dialog Manager can read it when enable

---
+++ Assets/Scripts/DialogSystem/Dialog.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialog", menuName ="Create a new Dialog",order = 0)]
+public class Dialog : ScriptableObject
+{
+    public string[] messages;
+}
---
+++ Assets/Scripts/DialogSystem/DialogManager.cs
@@ -0,0 +1,77 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public Dialog dialog;
+
+    public bool startOnAwake = false;
+
+    GameObject gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.instance.gameObject;
+        NextMessage();
+    }
+
+
+    public float lettersAppearSpeedProSec = 2;
+    int lastSchownLetterIndex = 0, currentMessageLenght = 0;
+    int currentMessageIndex = -1;
+    string nextMessage;
+
+    float t;
+
+    private void Awake()
+    {
+        if (!startOnAwake)
+            gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(gameManager.activeSelf)
+        {
+            t += Time.deltaTime;
+            int appearedLettersCount = (int) (t * lettersAppearSpeedProSec);
+            if(appearedLettersCount > lastSchownLetterIndex && currentMessageLenght > appearedLettersCount)
+            {
+                lastSchownLetterIndex = appearedLettersCount;
+                UpdateMessage();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+                NextMessage();
+        }
+    }
+
+    public void NextMessage()
+    {
+        if (lastSchownLetterIndex < currentMessageLenght - 1)
+        {
+            lastSchownLetterIndex = currentMessageLenght - 1;
+        }
+        else
+        {
+            if(currentMessageIndex == dialog.messages.Length - 1)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            lastSchownLetterIndex = -1;
+            nextMessage = dialog.messages[++currentMessageIndex];
+            currentMessageLenght = nextMessage.Length;
+            t = 0;
+        }
+        UpdateMessage();
+    }
+
+    void UpdateMessage()
+    {
+        text.text = nextMessage.Substring(0, lastSchownLetterIndex + 1);
+    }
+
+}
